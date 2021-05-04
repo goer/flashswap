325b467814d44ca468dfff7b8863c48e190807f8
SamWarden <SamWardenSad@gmail.com>
2021-05-04 18:57:00 +0300

    app/types.ts

---
+++ app/types.ts
@@ -0,0 +1,34 @@
+import type { TransactionResponse, TransactionReceipt, Log } from "@ethersproject/abstract-provider";
+
+export interface Event extends Log {
+  event: string;
+  args: Array<any>;
+}
+
+export interface TransactionReceiptWithEvents extends TransactionReceipt {
+  events?: Array<Event>;
+}
+
+export interface ContractFactory<ContractType> {
+  attach(address: string): ContractType;
+  deploy(...args: any[]): ContractType;
+}
+
+export interface ContractData {
+  _format: string,
+  abi: Array<any>,
+  contractName: string,
+  bytecode: string,
+  sourceName: string,
+  deployedBytecode: string,
+  linkReferences: object,
+  deployedLinkReferences: object,
+}
+
+export interface CoinData {
+  id: string,
+  symbol: string,
+  name: string,
+}
+
+// export { TransactionReceipt, TransactionResponse };
