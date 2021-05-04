9995605d4fd5647bae5774b6e105630b73e49096
CREESTL <kroymw3@yandex.ru>
2021-05-04 20:14:04 +0300

    Update Compiler Version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,7 +26,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.1",
+        version: "0.6.10",
         settings: {
           optimizer: {runs: 1, enabled: true},
         },
