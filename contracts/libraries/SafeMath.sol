024ec68af9105499b938ea06192cb6af99b8c346
SamWarden <SamWardenSad@gmail.com>
2021-05-05 17:16:17 +0300

    Add div method to SafeMath

--- contracts/libraries/SafeMath.sol
+++ contracts/libraries/SafeMath.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
 
-// a library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
+pragma solidity ^0.6.0;
 
 library SafeMath {
     function add(uint x, uint y) internal pure returns (uint z) {
@@ -13,5 +13,10 @@ library SafeMath {
 
     function mul(uint x, uint y) internal pure returns (uint z) {
         require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
+    }
+
+    function div(uint x, uint y) internal pure returns (uint z) {
+        require(b > 0, "SafeMath: division by zero");
+        z = a / b;
     }
 }
